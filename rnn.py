023b9ae8b6910af6dba33c777d59eb6f9fdeb49e
pdzhu <1140295051@qq.com>
2019-10-30 19:38:42 +0800

    网络1030

--- rnn.py
+++ rnn.py
@@ -1,43 +1,48 @@
+# coding=utf-8
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-from keras.models import load_model
 from keras.utils import np_utils
-from imblearn.over_sampling import SMOTE
 import time
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 # Import dataset
-dataset = pd.read_csv('/home/pdzhu/GestureRecognitionSourceFiles/one_file.csv', header=None)
+dataset = pd.read_csv('/home/pdzhu/GestureRecognitionSourceFiles/one_file3.csv', header=None)
+# dataset = pd.read_csv('/home/pdzhu/GestureRecognitionSourceFiles/CSV_la/1.csv', header=None)
 
 # Split to Data samples and labels
-x = dataset.iloc[:, 1:11].values
-y = dataset.iloc[:, 11].values
+x = dataset.iloc[:, 1:49].values
+y = dataset.iloc[:, 50].values
+
 
 # Number of time series to work on is 60
-t = 60
+t = 150
 n_samples = len(dataset)/t
 
 
 # Reshape input samples to Nx60x11
 x = np.reshape(x, (x.shape[0]/t, t, x.shape[1]))
+# x = np.reshape(x, (x.shape[0], len(dataset), x.shape[1]))
+
+
 # Set labels to these N samples
 indices = [ind for ind in range(len(y)) if ind % t == 0]
 y = y[indices]
+# print y
 
 # One-hot encode the classes
-
+#
 label_encoder = LabelEncoder()
 y_new = label_encoder.fit_transform(y)
 y_one_hot = np_utils.to_categorical(y_new)
 y = y_one_hot
-
+# print y
 # Split data to training and testing data
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)   # 训练集和测试集的分割
 
 # Construct the model
 n_neurons = 9
@@ -46,15 +51,16 @@ model = Sequential()
 model.add(LSTM(n_neurons, input_shape=(X_train.shape[1], X_train.shape[2])))
 model.add(Dense(output_dim=2, init='uniform', activation='sigmoid'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
+# print X_train.shape[1]
+# print X_train.shape[2]
 # Train the model
-model.fit(X_train, y_train, epochs=10, batch_size=7)
+model.fit(X_train, y_train, epochs=100, batch_size=7)
 
 # Evaluation of the Training time
 print "%s Minutes of Execution" % str((time.time()-start_time)/60)
 
 # Save the model for prediction
-model.save('model_test9_7pip.h5')
+model.save('model_test3.h5')
 print "Model Saved"
 print model.summary()
 
