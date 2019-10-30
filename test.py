8f7b08dec5e4d6a9e703866dd4d5d34342779605
pdzhu <1140295051@qq.com>
2019-10-30 19:33:22 +0800

    采集数据

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import Leap
 import time
 import pandas as pd
 
-data = [[] for _ in range(26)]  # 12这个数字可以改,表示有几个参数
+data = [[] for _ in range(51)]  # 12这个数字可以改,表示有几个参数
 init_time = time.time()
 
 
@@ -31,7 +31,6 @@ class SimpleListener(Leap.Listener):
                 else:
                     rightHand = h
 
-        print leftHand.fingers[1].joint_position(3)
         data[0].append(frame.id)
         data[1].append(time.time() - init_time)
         # 手掌向下角度   -180 ~ 180
@@ -71,6 +70,47 @@ class SimpleListener(Leap.Listener):
         data[24].append(leftHand.fingers[4].joint_position(3)[1])
         data[25].append(leftHand.fingers[4].joint_position(3)[2])
 
+        data[26].append(rightHand.palm_normal.yaw * Leap.RAD_TO_DEG)
+        data[27].append(rightHand.palm_normal.pitch * Leap.RAD_TO_DEG)
+        data[28].append(rightHand.palm_normal.roll * Leap.RAD_TO_DEG)
+
+        #
+        #  手掌位置  x  y  z   可考虑stabilized_palm_position
+        data[29].append(rightHand.palm_position[0])
+        data[30].append(rightHand.palm_position[1])
+        data[31].append(rightHand.palm_position[2])
+
+        #  手掌速度  x y z
+        #  palm_width   手掌宽度  后期可考虑去除手掌大小影响
+        data[32].append(rightHand.palm_velocity[0])
+        data[33].append(rightHand.palm_velocity[1])
+        data[34].append(rightHand.palm_velocity[2])
+
+        # 指尖位置  x y z
+        data[35].append(rightHand.fingers[0].joint_position(3)[0])
+        data[36].append(rightHand.fingers[0].joint_position(3)[1])
+        data[37].append(rightHand.fingers[0].joint_position(3)[2])
+
+        data[38].append(rightHand.fingers[1].joint_position(3)[0])
+        data[39].append(rightHand.fingers[1].joint_position(3)[1])
+        data[40].append(rightHand.fingers[1].joint_position(3)[2])
+
+        data[41].append(rightHand.fingers[2].joint_position(3)[0])
+        data[42].append(rightHand.fingers[2].joint_position(3)[1])
+        data[43].append(rightHand.fingers[2].joint_position(3)[2])
+
+        data[44].append(rightHand.fingers[3].joint_position(3)[0])
+        data[45].append(rightHand.fingers[3].joint_position(3)[1])
+        data[46].append(rightHand.fingers[3].joint_position(3)[2])
+
+        data[47].append(rightHand.fingers[4].joint_position(3)[0])
+        data[48].append(rightHand.fingers[4].joint_position(3)[1])
+        data[49].append(rightHand.fingers[4].joint_position(3)[2])
+        data[50].append(2)
+
+
+
+
 
 def main():
     # Create a sample listener and controller
@@ -98,7 +138,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+    for i in data:
+        print
+
     df = pd.DataFrame(data=data)
     df = df.transpose()
-    df.to_csv("./CSV_la/%s.csv" % (str(3)), sep=',', index=False, header=False)
-
+    df.to_csv("./CSV_1/%s.csv" % (str(200)), sep=',', index=False, header=False)
